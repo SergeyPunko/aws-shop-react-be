8aac37866c6d4e1c1a8246606562c779f4e9503d
Sergey Punko <punkosergey1997@gmail.com>
2023-06-08 06:48:29 +0300

    Init

--- handlers/dataBase/methods.ts
+++
@@ -1,6 +0,0 @@
-import * as products from './store.json';
-
-export const getProducts = async () => products;
-
-export const getProductById = async (id: string | undefined) =>
-    products.find((product) => product.id === id);
